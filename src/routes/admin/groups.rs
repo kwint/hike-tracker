fc3e7d1f1b5d37541eb7a034b71c35c2c3b672a2
Quint van Dijk <quintvdijk@gmail.com>
2026-01-27 21:29:53 +0100

    Removed unused group admin endpoints

--- src/routes/admin/groups.rs
+++ src/routes/admin/groups.rs
@@ -1,7 +1,5 @@
-use chrono::Utc;
 use image::{ImageEncoder, Luma};
 use qrcode::QrCode;
-use rocket::form::Form;
 use rocket::http::ContentType;
 use rocket::response::Redirect;
 use rocket::Route;
@@ -9,15 +7,8 @@ use rocket_dyn_templates::{context, Template};
 
 use crate::auth::Admin;
 use crate::db::DbConn;
-use crate::models::{Group, NewGroup};
+use crate::models::{Group};
 
-#[derive(FromForm)]
-pub struct NewGroupForm {
-    name: String,
-    scout_group: String,
-    members: String,
-    phone_number: String,
-}
 
 #[get("/")]
 pub async fn groups(_admin: Admin, conn: DbConn) -> Template {
@@ -25,46 +16,9 @@ pub async fn groups(_admin: Admin, conn: DbConn) -> Template {
     Template::render("admin/groups", context! { groups: groups, is_admin: true })
 }
 
-#[post("/", data = "<form>")]
-pub async fn create_group(_admin: Admin, conn: DbConn, form: Form<NewGroupForm>) -> Redirect {
-    let name = form.name.clone();
-    let scout_group = form.scout_group.clone();
-    let members = form.members.clone();
-    let phone_number = form.phone_number.clone();
-    let result = conn
-        .run(move |c| {
-            let group = NewGroup::new(name, scout_group, members, phone_number);
-            Group::insert(c, group)
-        })
-        .await;
-    if let Err(e) = result {
-        eprintln!("Failed to create group: {}", e);
-    }
-
-    Redirect::to("/admin/groups")
-}
-
 #[get("/<id>/delete")]
 pub async fn delete_group(_admin: Admin, conn: DbConn, id: String) -> Redirect {
     conn.run(move |c| Group::delete(c, &id)).await.ok();
-    Redirect::to("/admin/groups")
-}
-
-#[get("/<id>/start_timer")]
-pub async fn start_group_timer(_admin: Admin, conn: DbConn, id: String) -> Redirect {
-    let now = Utc::now().naive_utc();
-    conn.run(move |c| Group::set_start_time(c, &id, now))
-        .await
-        .ok();
-    Redirect::to("/admin/groups")
-}
-
-#[get("/<id>/stop_timer")]
-pub async fn stop_group_timer(_admin: Admin, conn: DbConn, id: String) -> Redirect {
-    let now = Utc::now().naive_utc();
-    conn.run(move |c| Group::set_finish_time(c, &id, now))
-        .await
-        .ok();
     Redirect::to("/admin/groups")
 }
 
@@ -92,9 +46,6 @@ pub fn group_qr(_admin: Admin, id: &str) -> (ContentType, Vec<u8>) {
 pub fn routes() -> Vec<Route> {
     routes![
         groups,
-        create_group,
-        start_group_timer,
-        stop_group_timer,
         delete_group,
         group_qr
     ]
